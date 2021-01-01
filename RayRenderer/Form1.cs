e32511086e9f7e391c40da646b6b9dfead3ab6c4
blockplacer <53020477+blockplacer@users.noreply.github.com>
2021-01-02 02:14:22 +0300

    Add files via upload

--- RayRenderer/Form1.cs
+++ RayRenderer/Form1.cs
@@ -27,7 +27,7 @@ namespace RayRenderer
         {
            float vec = RY.dist(RY.position, RY.Vector3(0.0f, 0.0f, 20.0f));
             float vec_ = RY.dist(RY.position, RY.Vector3(0.0f, 0.0f, 30.0f)); //grdient object
-
+            RY.initGraphics(e.Graphics);
             if (vec < 130)
             {
                 /* for (int xj = 0; xj <20f; xj++)
@@ -35,7 +35,7 @@ namespace RayRenderer
                      e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(xj,0,1,1)), new Rectangle(new Point(pictureBox1.Width / 2 + (int)x_ + 80, pictureBox1.Height / 2 + (int)RY.position.Y + xj), new Size((int)vec, (int)vec)));
                  }
                  */
-                RY.shapes.rectngularpyrism(RY.Vector3(pictureBox1.Width / 2 + (int)RY.x_, pictureBox1.Height / 2, -30.0f), new Size((int)vec, (int)vec + 10), Color.BlueViolet, e.Graphics);//
+                RY.shapes.rectngularpyrism(RY.Vector3(pictureBox1.Width / 2 + (int)RY.x_, pictureBox1.Height / 2, -30.0f), new Size((int)vec, (int)vec + 10), Color.BlueViolet);//
 
                 // RY.Side
                 //  e.Graphics.FillRectangle(new SolidBrush(Color.Purple), new Rectangle(new Point(pictureBox1.Width / 2+(int)x_+80, pictureBox1.Height / 2+(int) RY.position.Y), new Size((int)vec, (int)vec)));
@@ -43,7 +43,7 @@ namespace RayRenderer
 
 
             if (vec_ < 130)//
-                RY.shapes.rectngularpyrism(RY.Vector3(pictureBox1.Width / 2 + (int)RY.x_+30, pictureBox1.Height / 2, -20.0f), new Size((int)vec_, (int)vec_+10 ), Color.Orange, e.Graphics);//+ 10
+                RY.shapes.rectngularpyrism(RY.Vector3(pictureBox1.Width / 2 + (int)RY.x_+30, pictureBox1.Height / 2, -20.0f), new Size((int)vec_, (int)vec_+10 ), Color.Orange);//+ 10
             // e.Graphics.FillRectangle(new SolidBrush(Color.Cyan), new Rectangle(new Point(pictureBox1.Width / 2+(int)x_, pictureBox1.Height / 2+(int)RY.position.Y ), new Size((int)vec_, (int)vec_)));
            
 
--- RayRenderer/RY.cs
+++ RayRenderer/RY.cs
@@ -23,34 +23,46 @@ namespace RayRenderer
         {
             return new Vector3(x, y, z);
         }
-        public static void Side(Vector3 vector3,Graphics graphics)
+        public static Graphics graphics;
+        public static void initGraphics(Graphics graphic)
+        {
+            graphics = graphic;
+        }
+        public static void Side(Vector3 vector3,Size size,Color color)
         {
             graphics.TranslateTransform(vector3.X, vector3.Y);
-            Point[] points = {new Point(300, 41),
-    new Point(300, 1),///(int)position.Z
-    new Point(315, 1+(int)RY.position.Z),
-            new Point(315, 41-(int)RY.position.Z)};//[]
-           graphics.FillPolygon(new SolidBrush(Color.Orange), points);
-            graphics.TranslateTransform(0, 0);
+            Point[] points = {new Point(300, size.Height),
+    new Point(300, 1),/////(int)position.Z
+    new Point(315, 1+(int)RY.position.Z),//+size.Height size.Height-
+            new Point(315, size.Height-(int)RY.position.Z)};//[]
+                                                            //81+
+            graphics.FillPolygon(new SolidBrush(color), points);
+            graphics.ResetTransform();
+           // graphics.TranslateTransform(0, 0);
+            //81+
         }
         public static class shapes
         {
-            public static void rectngularpyrism(Vector3 vector3, Size size,Color color ,Graphics graphics)//,
+
+            public static void rectngularpyrism(Vector3 vector3, Size size,Color color)//,
             {
 
 
                 PointF[] points = {    new PointF(position.X-size.Width, 1f-position.Y-size.Height),
 new PointF(position.X-size.Width, size.Height), new PointF(27f+size.Width, size.Height), new PointF(26f+size.Width, 2f-position.Y-size.Height)};
 
-               
+                float vec = dist(position, vector3);//new RayRenderer.Vector3(0.0f,0.0f,-30.0f)
+                Side(RY.Vector3((int)vector3.X/2, (int)vector3.Y/2, (int)vector3.Z), new Size((int)vec, (int)vec / 3), Color.Purple);//graphics
                 graphics.TranslateTransform(vector3.X, vector3.Y);
 
-
+                
                 graphics.FillPolygon(new SolidBrush(color), points);
 
                 //  graphics.FillRectangle(new SolidBrush(Color.BlueViolet), new Rectangle(new Point((int)-vector3.X,(int)vector3.Y),size));//new Size()
                 //  graphics.FillRectangle(new SolidBrush(Color.BlueViolet), new Rectangle(new Point((int)vector3.X, (int)vector3.Y), size));
                 //  graphics.FillRectangle(new SolidBrush(Color.BlueViolet), new Rectangle(new Point(20+(int)vector3.X, (int)vector3.Y), size));
+               
+                //new Size()
 
                 graphics.ResetTransform();
                 // graphics.TranslateTransform(0, 0);
